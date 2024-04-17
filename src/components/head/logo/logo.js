a53f6875584fffe4e3c1a733dba8ab95bfa460f5
Крепакова Мария <krepakova@sfedu.ru>
2024-04-17 18:00:09 +0300

    17.04

---
+++ src/components/head/logo/logo.js
@@ -0,0 +1,9 @@
+import "./logo.css"
+
+export default function Logo(){
+    return(
+        <div classname = "logo">
+            <p>Сайт создан в 2024 &copy; Крепакова</p>
+        </div>
+    )
+}
